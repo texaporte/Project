e74f59d9751aa6fa8a90d0e0a6f88684e2653ade
Brian Mondido <bmondido@student.neumont.edu>
2012-05-23 20:30:45 -0600

    Derp

--- src/edu/neumont/mat305/Sample.java
+++ src/edu/neumont/mat305/Sample.java
@@ -2,6 +2,7 @@ package edu.neumont.mat305;
 
 public class Sample {
 	public static void main(String[] args) {
-		System.out.println("I'm making a change.");
+		System.out.println("I'm making a chadasfdafnge.");
+		System.out.println("djfksl");
 	}
 }
